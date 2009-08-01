9d6c1688dfc93eca7de9b92928f1bffb77dfb24e
Admin <admin@chrisoldwood.com>
2009-08-01 23:17:24 +0000

    Updated as the tests are now self-registering.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -7,29 +7,7 @@
 #include <tchar.h>
 #include <Core/UnitTest.hpp>
 
-////////////////////////////////////////////////////////////////////////////////
-// The test group functions.
-
-extern void TestComUtils();
-extern void TestObjectBase();
-extern void TestInprocServer();
-extern void TestClassFactory();
-extern void TestErrorInfo();
-
-////////////////////////////////////////////////////////////////////////////////
-//! The entry point for the test harness.
-
 int _tmain(int argc, _TCHAR* argv[])
 {
-	TEST_SUITE_BEGIN(argc, argv)
-	{
-		TEST_CASE(TestComUtils);
-		TEST_CASE(TestObjectBase);
-		TEST_CASE(TestInprocServer);
-		TEST_CASE(TestClassFactory);
-		TEST_CASE(TestErrorInfo);
-
-		Core::setTestRunFinalStatus(true);
-	}
-	TEST_SUITE_END
+	TEST_SUITE_MAIN(argc, argv);
 }
